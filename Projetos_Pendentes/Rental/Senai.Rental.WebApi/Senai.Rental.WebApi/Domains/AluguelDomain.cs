77720e777d5a3130cc434a8b62b1711f0e03f645
Your Name <feliperodriguew285@gmail.com>
2021-12-01 23:48:09 -0300

    organizando mais

---
+++ Projetos_Pendentes/Rental/Senai.Rental.WebApi/Senai.Rental.WebApi/Domains/AluguelDomain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Rental.WebApi.Domains
+{
+    public class AluguelDomain
+    {
+        public int idAluguel { get; set; }
+        public int idEmpresa { get; set; }
+        public int idCliente { get; set; }
+        public int idVeiculo { get; set; }
+        public EmpresaDomain EmpresaDomain { get; set; }
+        public ClienteDomain ClienteDomain { get; set; }
+        public VeiculoDomain VeiculoDomain { get; set; }
+        public DateTime DataDev { get; set; }
+        public DateTime DataAluguel { get; set; }
+    }
+}
